483336de8b366afe6debd766593c25b76bfc685f
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-02 14:34:23 +0200

    Update daily.js

--- routes/api/daily.js
+++ routes/api/daily.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const controllers = require("../../controllers/daily");
 const { dailyRateTemplate } = require("../../controllers/daily/getDailyRate");
 const { asyncWrapper } = require("../../utils");
-const { validateBody } = require("../../middleware");
+const { validateBody, auth, isValidId } = require("../../middleware");
 
 router.post(
   "/daily-rate",
   validateBody(dailyRateTemplate),
   asyncWrapper(controllers.getDailyRate)
+);
+router.patch(
+  "/daily-rate/:id",
+  isValidId,
+  auth,
+  validateBody(dailyRateTemplate),
+  asyncWrapper(controllers.getDailyRateUser)
 );
 
 module.exports = router;
