b56313413237d3242ca365037d161dfa5d8c828f
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-02 14:34:18 +0200

    Update google-strategy.js

--- middleware/strategies/google-strategy.js
+++ middleware/strategies/google-strategy.js
@@ -1,10 +1,10 @@
 const { Strategy } = require("passport-google-oauth2");
 const { User } = require("../../models/user");
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, ARR_URL } =
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, APP_URL } =
   process.env;
 
-const callbackURL = `${ARR_URL}${GOOGLE_CALLBACK_URL}`;
+const callbackURL = `${APP_URL}${GOOGLE_CALLBACK_URL}`;
 
 const googleParams = {
   clientID: `${GOOGLE_CLIENT_ID}`,
@@ -26,7 +26,10 @@ const googleCallback = async (
     if (user) {
       return done(null, user);
     }
-    const newUser = await User.create({ email, name: displayName });
+    const newUser = await User.create({
+      email,
+      name: displayName,
+    });
     done(null, newUser);
   } catch (error) {
     done(error, false);
