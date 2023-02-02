11fcf3e45958ab9b05b3534c0c0d9dc7a80df01c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-02 14:33:47 +0200

    Update logout.js

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -4,6 +4,6 @@ async function logout(req, res) {
   const { _id } = req.user;
 
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204).json({});
+  res.status(200).json({ message: "Done✅" });
 }
 module.exports = logout;
