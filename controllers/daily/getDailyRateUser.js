b2996e90d9510c3923fee45165a48cdeb59db70b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-02 14:33:58 +0200

    Create getDailyRateUser.js

---
+++ controllers/daily/getDailyRateUser.js
@@ -0,0 +1,20 @@
+const { User } = require("../../models/user");
+const countDailyRate = require("../../service/countDailyRate");
+const notAllowedProducts = require("../../db/notAllowedProducts");
+
+async function getDailyRateUser(req, res) {
+  const { id } = req.params;
+  const { body, user } = req;
+
+  const dailyRate = await countDailyRate(body);
+
+  const result = await User.findByIdAndUpdate(id, {
+    healthyData: { bodyCalculating: body, dailyRate, notAllowedProducts },
+  });
+
+  res.status(200).json({
+    id: result._id,
+    healthyData: result.healthyData,
+  });
+}
+module.exports = getDailyRateUser;
