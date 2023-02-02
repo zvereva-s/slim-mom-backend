7e1454d3655d720f05967cd1d1b064ff7af40e78
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-02 14:34:14 +0200

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const auth = require("./auth");
+const isValidId = require("./isValidId");
 const authenticateSocial = require("./authenticate-social");
 
 module.exports = {
   validateBody,
   auth,
   authenticateSocial,
+  isValidId,
 };
