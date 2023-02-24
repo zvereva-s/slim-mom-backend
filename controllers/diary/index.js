05f43ca53fcb14b1c012029c57fe011c355f7d62
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-24 21:53:01 +0200

    Create index.js

---
+++ controllers/diary/index.js
@@ -0,0 +1,12 @@
+const getProductDiaryList = require("./getProductDiaryList");
+const addProduct = require("./addProduct");
+const deleteProduct = require("./deleteProduct");
+
+const getFoodList = require("./getFoodList");
+
+module.exports = {
+  getProductDiaryList,
+  addProduct,
+  deleteProduct,
+  getFoodList,
+};
