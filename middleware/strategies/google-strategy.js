c88451fd5ca0861a8e33a5385008253faf299bdb
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-24 21:53:25 +0200

    Update google-strategy.js

--- middleware/strategies/google-strategy.js
+++ middleware/strategies/google-strategy.js
@@ -1,5 +1,7 @@
 const { Strategy } = require("passport-google-oauth2");
 const { User } = require("../../models/user");
+const { HealthyData } = require("../../models/healthyData");
+
 const notAllowedProducts = require("../../db/notAllowedProducts");
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, APP_URL } =
@@ -24,27 +26,32 @@ const googleCallback = async (
   try {
     const { email, displayName } = profile;
     const user = await User.findOne({ email });
+
+    let healthyData = null;
     if (user) {
-      return done(null, user);
+      const owner = user._id;
+      healthyData = await HealthyData.findOne({ owner });
+      return done(null, user, healthyData);
     }
     const newUser = await User.create({
       email,
       name: displayName,
-      healthyData: {
-        notAllowedProducts,
-        bodyCalculating: {
-          age: 0,
-          gender: "",
-          height: 0,
-          physicalActivity: 0,
-          weight: 0,
-          bloodType: 0,
-          desiredWeight: 0,
-        },
-        dailyRate: "0",
-      },
     });
-    done(null, newUser);
+    healthyData = await HealthyData.create({
+      owner: newUser._id,
+      notAllowedProducts,
+      bodyCalculating: {
+        age: 0,
+        gender: "",
+        height: 0,
+        physicalActivity: 0,
+        weight: 0,
+        bloodType: 0,
+        desiredWeight: 0,
+      },
+      dailyRate: "0",
+    });
+    done(null, newUser, healthyData);
   } catch (error) {
     done(error, false);
   }
