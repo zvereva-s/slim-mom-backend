f3d317794130de8fd9fcb2fc7724fe436e100adf
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-24 21:52:35 +0200

    Update signin.js

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
 const { User } = require("../../models/user");
+const { HealthyData } = require("../../models/healthyData");
 const { requestError } = require("../../utils");
 
 const { SECRET_KEY } = process.env;
@@ -8,6 +10,9 @@ const { SECRET_KEY } = process.env;
 async function signin(req, res) {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
+  const owner = user._id;
+  const healthyData = await HealthyData.findOne({ owner });
 
   if (!user) {
     throw requestError(401, "Email or password is wrong");
@@ -30,7 +35,12 @@ async function signin(req, res) {
       id: user._id,
       name: user.name,
       email: user.email,
-      healthyData: user.healthyData,
+    },
+    healthyData: {
+      owner: healthyData.owner,
+      notAllowedProducts: healthyData.notAllowedProducts,
+      bodyCalculating: healthyData.bodyCalculating,
+      dailyRate: healthyData.dailyRate,
     },
   });
 }
